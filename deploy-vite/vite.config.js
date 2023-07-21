81690a03113ad1d4f4ff3b6a30178836e1517c61
Sara MCClelland <saramcclelland@icloud.com>
2023-07-21 18:50:25 +0100

    changes

--- deploy-vite/vite.config.js
+++ deploy-vite/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/deploy-vite/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./src/main.jsx",
+      },
+    },
+  },
 })
